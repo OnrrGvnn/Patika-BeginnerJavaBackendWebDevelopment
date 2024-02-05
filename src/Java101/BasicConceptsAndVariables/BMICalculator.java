b79db3031e21304d7bfc74b4fd5732bae63edf0b
OnrrGvnn <158863872+OnrrGvnn@users.noreply.github.com>
2024-02-05 20:38:11 +0300

    BMI Calculator Completed.

---
+++ src/Java101/BasicConceptsAndVariables/BMICalculator.java
@@ -0,0 +1,26 @@
+// Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın. Kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+
+package Java101.BasicConceptsAndVariables;
+
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double height, weight, bmi;
+
+        System.out.println();
+        System.out.print("Please Enter Your Height (in meters) : ");
+        height = input.nextDouble();
+        System.out.print("Please Enter Your Weight (in kg) : ");
+        weight = input.nextDouble();
+
+        bmi = (weight / (height * height));
+
+        System.out.println();
+        System.out.println("Your Body Mass Index : " + bmi);
+
+    }
+
+}
