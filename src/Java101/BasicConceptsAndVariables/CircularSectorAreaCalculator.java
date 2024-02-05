bd7cdf664fbdb8b5b71787061cbc82dd7a92c369
OnrrGvnn <158863872+OnrrGvnn@users.noreply.github.com>
2024-02-05 20:02:10 +0300

    Circular Sector Area Calculator Completed.

---
+++ src/Java101/BasicConceptsAndVariables/CircularSectorAreaCalculator.java
@@ -0,0 +1,26 @@
+// Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
+
+package Java101.BasicConceptsAndVariables;
+
+import java.util.Scanner;
+
+public class CircularSectorAreaCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double r, a, area, pi = 3.14;
+
+        System.out.println();
+        System.out.print("Enter The Radius Of The Circle : ");
+        r = input.nextDouble();
+        System.out.print("Enter The Measure Of The Central Angle : ");
+        a = input.nextDouble();
+
+        area = ((pi * (r * r) * a) / 360);
+
+        System.out.println();
+        System.out.println("Circular Sector Area : " + area);
+
+    }
+
+}
