4be4f614b753b8e413a218b5cdafd9d58cd8a5c9
OnrrGvnn <158863872+OnrrGvnn@users.noreply.github.com>
2024-02-05 16:41:39 +0300

    KDV Calculator completed.

---
+++ src/Java101/BasicConceptsAndVariables/KDVCalculator.java
@@ -0,0 +1,26 @@
+/* Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran programı yazın.
+Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.  */
+
+package Java101.BasicConceptsAndVariables;
+
+import java.util.Scanner;
+
+public class KDVCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double price, kdvPrice, total;
+
+        System.out.println();
+        System.out.print("Please Enter The Price : ");
+        price = input.nextDouble();
+
+        kdvPrice = (price > 0 && price <= 1000) ? (0.18 * price) : (0.08 * price);
+        System.out.println();
+        System.out.println("KDV Price : " + kdvPrice);
+
+        total = (price + kdvPrice);
+        System.out.println("Total Price : " + total);
+
+    }
+}
