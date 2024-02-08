59dad24fb3a3210003359918bb3f52a009503d0c
OnrrGvnn <158863872+OnrrGvnn@users.noreply.github.com>
2024-02-08 15:15:39 +0300

    Triangle Area Calculator Completed.

--- src/Java101/BasicConceptsAndVariables/TriangleAreaCalculator.java
+++ src/Java101/BasicConceptsAndVariables/TriangleAreaCalculator.java
@@ -8,22 +8,20 @@ public class TriangleAreaCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int side1, side2, side3, u;
-        double area;
+        int side1, side2, side3;
+        double u, area;
 
-        System.out.println();
-        System.out.print("Enter The First Side Of The Triangle : ");
+        System.out.print("\nEnter The First Side Of The Triangle : ");
         side1 = input.nextInt();
         System.out.print("Enter The Second Side Of The Triangle : ");
         side2 = input.nextInt();
         System.out.print("Enter The Third Side Of The Triangle : ");
         side3 = input.nextInt();
 
-        u = (side1 + side2 + side3) / 2;
+        u = (double) (side1 + side2 + side3) / 2;
         area = Math.sqrt(u * (u - side1) * (u - side2) * (u - side3));
 
-        System.out.println();
-        System.out.println("Triangle Area : " + area);
+        System.out.println("\nTriangle Area : " + area);
 
     }
 
