890be70518e2f20c5dcec791d0e5b64f0078f6be
OnrrGvnn <158863872+OnrrGvnn@users.noreply.github.com>
2024-02-13 04:11:13 +0300

    Star Diamond Printer Completed.

---
+++ src/Java101/Loops/StarDiamondPrinter.java
@@ -0,0 +1,49 @@
+// Java'da döngüler kullanarak yıldızlar ile elmas yapınız.
+
+package Java101.Loops;
+
+import java.util.Scanner;
+
+public class StarDiamondPrinter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int userNumber;
+
+        System.out.print("\nHow Many Layers Of Stars? : ");
+        userNumber = input.nextInt();
+        System.out.println();
+
+        if (userNumber > 0) {
+            for (int i = 0; i <= userNumber; i++) {
+                for (int j = 1; j <= userNumber - i; j++) {
+                    System.out.print(" ");
+                }
+
+                for (int k = 1; k <= ((2 * i) + 1); k++) {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+
+            }
+
+            for (int i = 0; i <= userNumber; i++) {
+                for (int j = 0; j <= i; j++) {
+                    System.out.print(" ");
+                }
+
+                for (int k = 1; k <= ((2 * (userNumber - i) - 1)); k++) {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+
+            }
+        } else {
+            System.out.println("\nYou Made An Invalid Entry. Please Try Again!");
+        }
+
+    }
+    
+}
