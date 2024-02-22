8ccdf347729bdc23ed6fd9d528448c3acc3dd602
OnrrGvnn <158863872+OnrrGvnn@users.noreply.github.com>
2024-02-22 16:02:36 +0300

    Harmonic Mean Calculator Completed.

---
+++ src/Java101/Arrays/HarmonicMeanCalculator.java
@@ -0,0 +1,19 @@
+package Java101.Arrays;
+
+public class HarmonicMeanCalculator {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        double sum = 0.0;
+        double harMean;
+
+        for (double i : numbers) {
+            sum += (1 / i);
+        }
+
+        harMean = (double) numbers.length / sum;
+
+        System.out.println("\nHarmonic Mean : " + harMean);
+
+    }
+
+}
